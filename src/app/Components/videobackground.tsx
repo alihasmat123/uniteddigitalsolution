631f07d41844071ad4f1d91e35bcd970114a9e5e
Hasmat Ali <hasmat20002000@gmail.com>
2024-12-23 09:04:37 +0000

    video codec solved

--- src/app/Components/videobackground.tsx
+++ src/app/Components/videobackground.tsx
@@ -2,10 +2,17 @@
 
 export default function VideoBackground() {
   return (
-    <video autoPlay loop muted playsInline className="absolute w-full h-full object-cover px-0 py-0">
-      <source src="/assets/weavy.m4v" type="video/mp4; codecs=hvc1" />
-      <source src="/assets/weavy.webm" type="video/webm; codecs=vp9" />
-      Your browser does not support the video tag.
+    <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute w-full h-full object-cover px-0 py-0"
+        >
+        <source src="/assets/weavy.mp4" type="video/mp4" />
+        <source src="/assets/weavy.webm" type="video/webm" />
+        Your browser does not support the video tag.
     </video>
+
   );
 }
